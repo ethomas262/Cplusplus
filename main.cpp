3dcdbb0cf5d34613138b077d4355c41712bb804f
ethomas262 <122247620+ethomas262@users.noreply.github.com>
2023-06-25 15:28:40 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+#include "Bank.h"
+#include "Bank.cpp"
+
+using namespace std;
+
+int main(){
+    // declare variables
+
+    int response;
+    int annual_interest;
+    int num_of_years;
+    double initial_investement;
+    double monthly_deposit;
+    
+    //prompt user input
+
+
+    //create bank object
+
+
+    //display bank statements
+
+
+    // prompt user response
+
+
+
+    // switch response
+
+    return 0;
+}
